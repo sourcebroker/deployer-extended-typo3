103badc7bd3d458bb307061951a9f871a7afaa86
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-02-19 23:37:20 +0100

    [BUGFIX] Load deployer-extended only if running deployer

--- src/Autoload.php
+++ src/Autoload.php
@@ -1,11 +1,15 @@
 <?php
 
-\SourceBroker\DeployerExtended\Utility\FileUtility::requireFilesFromDirectoryReqursively(__DIR__ . '/../deployer/config/', '/^set_driver/');
-\SourceBroker\DeployerExtended\Utility\FileUtility::requireFilesFromDirectoryReqursively(__DIR__ . '/../deployer/task/');
+// run only if called from deployer.phar context
+if (function_exists('\Deployer\set')) {
 
-if ((new \SourceBroker\DeployerExtendedTypo3\Drivers\Typo3Driver)->detectConfig(\Deployer\get('current_dir'))) {
-    require_once(__DIR__ . '/../deployer/config/set_driver_default.php');
-}
-if ((new \SourceBroker\DeployerExtendedTypo3\Drivers\Typo3EnvDriver)->detectConfig(\Deployer\get('current_dir'))) {
-    require_once(__DIR__ . '/../deployer/config/set_driver_env.php');
+    \SourceBroker\DeployerExtended\Utility\FileUtility::requireFilesFromDirectoryReqursively(__DIR__ . '/../deployer/config/', '/^set_driver/');
+    \SourceBroker\DeployerExtended\Utility\FileUtility::requireFilesFromDirectoryReqursively(__DIR__ . '/../deployer/task/');
+
+    if ((new \SourceBroker\DeployerExtendedTypo3\Drivers\Typo3Driver)->detectConfig(\Deployer\get('current_dir'))) {
+        require_once(__DIR__ . '/../deployer/config/set_driver_default.php');
+    } elseif ((new \SourceBroker\DeployerExtendedTypo3\Drivers\Typo3EnvDriver)->detectConfig(\Deployer\get('current_dir'))) {
+        require_once(__DIR__ . '/../deployer/config/set_driver_env.php');
+    }
+
 }
