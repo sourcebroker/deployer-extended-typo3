e36a1ce926dffe20037b1bbf8b542adb4bbc5301
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-02-20 14:10:29 +0100

    [TASK] Use db:truncate from deployer-extended.

--- deployer/task/deploy.php
+++ deployer/task/deploy.php
@@ -17,7 +17,7 @@ task('deploy', [
     'deploy:clear_paths', // Remove files and folders which are not needed for webiste to work.
     'deploy:symlink', // Switch old php code with new version (./release/ dir is removed and all is now in ./current/ folder)
     'deployer:download', // Download deployer if its not there yet becase its needed to clear database cache tables.
-    'typo3:db:truncate_table', // Clear database cache tables.
+    'db:truncate_table_remote', // Clear database cache tables.
     'typo3:cache:delete_typo3temp_cache', // Delete all typo3temp cache files.
     'typo3:db:update_schema', // Update db schema for TYPO3.
     'cache:frontendreset',
