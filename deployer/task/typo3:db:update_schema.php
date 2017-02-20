c254670d7b607d0a2a53d148dffae498a77374e0
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-02-20 11:45:24 +0100

    [TASK] Cleanup for update schema

--- deployer/task/typo3:db:update_schema.php
+++ deployer/task/typo3:db:update_schema.php
@@ -5,15 +5,6 @@ namespace Deployer;
 // check if we use corapi vs typo3_console database upadate schema method
 task('typo3:db:update_schema', function(){
 
-    if (run("if [ -e {{release_path}}/vendor/bin/typo3cms ]; then echo 'true'; fi")->toBool()) {
-        $typo3CmsBin = get('release_path') . '/vendor/bin/typo3cms';
-    } else {
-        $typo3CmsBin = get('release_path') . '/typo3cms';
-    }
-    if(strpos(run('cd {{deploy_path}}/current && {{bin/php}} ' . $typo3CmsBin . ' databaseapi:databasecompare 2,3,4')->toString(), 'No command could be found') !== FALSE) {
-        run('cd {{deploy_path}}/current && {{bin/php}} ' . $typo3CmsBin . ' database:updateschema field.add,field.change,table.add,table.change');
-    } else {
-        run('cd {{deploy_path}}/current && {{bin/php}} ' . $typo3CmsBin . ' databaseapi:databasecompare 2,3,4');
-    }
+    run('cd {{deploy_path}}/current && {{bin/php}} ./vendor/bin/typo3cms database:updateschema field.add,field.change,table.add,table.change');
 
 })->desc('Updates database schema.');
