62d53aaacaae7462eb0eb7c76e6757e862f58acf
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-02-20 22:14:54 +0100

    [BUGFIX] addAfter to add becase addAfter bug

--- deployer/task/typo3:regenerate_dotenv.php
+++ deployer/task/typo3:regenerate_dotenv.php
@@ -7,4 +7,6 @@ task('typo3:regenerate_dotenv', function(){
     run("rm -f {{release_path}}/typo3temp/logs/dotenv-cache-*");
     run("{{bin/php}} {{release_path}}/vendor/bin/typo3cms");
 
-})->setPrivate()->desc('Updates database schema.')->addAfter('deploy:vendors');
+})->setPrivate()->desc('Updates database schema.');
+
+after('deploy:vendors', 'typo3:regenerate_dotenv');
