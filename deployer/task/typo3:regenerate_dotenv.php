afd57f732de91133cfb76b5a7e6adbdbb3b65f1b
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-03-04 11:19:30 +0100

    [TASK] Refactor env cache regeneration.

--- deployer/task/typo3:regenerate_dotenv.php
+++ deployer/task/typo3:regenerate_dotenv.php
@@ -2,11 +2,9 @@
 
 namespace Deployer;
 
-task('typo3:regenerate_dotenv', function(){
+task('typo3:regenerate_env_cache', function(){
 
     run("rm -f {{release_path}}/typo3temp/logs/dotenv-cache-*");
     run("cd {{release_path}} && {{bin/php}} {{bin/typo3cms}}");
 
-})->setPrivate()->desc('Updates database schema.');
-
-after('deploy:vendors', 'typo3:regenerate_dotenv');
+})->setPrivate()->desc('Regenerate .env cache after adding .env from shared.');
