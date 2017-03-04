1c970ff4775c29a2bec2693940588ca92bff9db9
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-03-04 11:18:18 +0100

    [TASK] Refactor task order in main deploy.

--- deployer/task/deploy.php
+++ deployer/task/deploy.php
@@ -12,16 +12,17 @@ task('deploy', [
     'deploy:shared',
     'deploy:writable',
     'deploy:vendors',
-    'typo3:regenerate_dotenv',
-    'lock:overwrite_entry_point',
-    'cache:clearstatcache',
-    'lock:create_lock_files', // No frontend access possbile from now. Requests are buffered.
     'deploy:clear_paths', // Remove files and folders which are not needed for webiste to work.
-    'deploy:symlink', // Switch old php code with new version (./release/ dir is removed and all is now in ./current/ folder)
+    'typo3:regenerate_dotenv',
+    'typo3:db:update_schema', // Update db schema for TYPO3.
+    'cache:clearstatcache',
     'deployer:download', // Download deployer if its not there yet becase its needed to clear database cache tables.
+    'lock:overwrite_entry_point',
+    'lock:create_lock_files', // No frontend access possbile from now. Requests are buffered.
+    'deploy:symlink', // Switch old php code with new version (./release/ dir is removed and all is now in ./current/ folder)
     'db:truncate', // Clear database cache tables.
     'typo3:cache:delete_typo3temp_cache', // Delete all typo3temp cache files.
-    'typo3:db:update_schema', // Update db schema for TYPO3.
+    'cache:clearstatcache',
     'cache:frontendreset',
     'lock:delete_lock_files', // Frontend access possbile again from now.
     'cleanup',
