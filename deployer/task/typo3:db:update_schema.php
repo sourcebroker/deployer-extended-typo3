d7bf3132a8417c5b69ef35d9f133d9a2f36d03d2
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-03-04 16:16:08 +0100

    [TASK] typo3:db:update_schema task cleanup

--- deployer/task/typo3:db:update_schema.php
+++ deployer/task/typo3:db:update_schema.php
@@ -2,9 +2,8 @@
 
 namespace Deployer;
 
-// check if we use corapi vs typo3_console database upadate schema method
 task('typo3:db:update_schema', function(){
 
     run('cd {{deploy_path}}/current && {{bin/php}} {{bin/typo3cms}} database:updateschema field.add,field.change,table.add,table.change');
 
-})->desc('Updates database schema.');
+})->desc('Updates TYPO3 database schema: typo3cms database:updateschema field.add,field.change,table.add,table.change');
