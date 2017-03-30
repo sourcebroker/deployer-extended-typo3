3b86e40c1a985ad3d68f1dba31ec2d6cc8189b58
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-03-31 00:06:30 +0200

    [TASK] make regenerate env quitet

--- deployer/task/typo3:regenerate_dotenv.php
+++ deployer/task/typo3:regenerate_dotenv.php
@@ -5,6 +5,6 @@ namespace Deployer;
 task('typo3:regenerate_env_cache', function(){
 
     run("rm -f {{release_path}}/typo3temp/logs/dotenv-cache-*");
-    run("cd {{release_path}} && {{bin/php}} {{bin/typo3cms}}");
+    run("cd {{release_path}} && {{bin/php}} {{bin/typo3cms}} 2>&1");
 
 })->setPrivate()->desc('Regenerate .env cache after adding .env from shared.');
