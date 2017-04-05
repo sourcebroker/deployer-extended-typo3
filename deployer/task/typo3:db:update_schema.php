b6e977829e8302cb3b251e8eacf920ef123e6629
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-04-05 12:13:43 +0200

    [BUGFIX] Run command to update db schema correctly depending on where is was called. In "release" or in current "folder"

--- deployer/task/typo3:db:update_schema.php
+++ deployer/task/typo3:db:update_schema.php
@@ -4,6 +4,11 @@ namespace Deployer;
 
 task('typo3:db:update_schema', function(){
 
-    run('cd {{deploy_path}}/current && {{bin/php}} {{bin/typo3cms}} database:updateschema field.add,field.change,table.add,table.change');
+    if (run('if [ -L {{deploy_path}}/release ] ; then echo true; fi')->toBool()) {
+        set('active_dir', get('deploy_path') . '/release');
+    } else {
+        set('active_dir', get('deploy_path') . '/current');
+    }
+    run('cd {{active_dir}} && {{bin/php}} {{bin/typo3cms}} database:updateschema field.add,field.change,table.add,table.change');
 
 })->desc('Updates TYPO3 database schema: typo3cms database:updateschema field.add,field.change,table.add,table.change');
