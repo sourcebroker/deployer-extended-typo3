6f29ff96dc9478328dcedc5e899b43c02292100c
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-05-14 13:22:52 +0200

    [TASK] Init version for TYPO3 8.7

--- deployer/cache/task/typo3:cache:delete_typo3temp_cache.php
+++ deployer/cache/task/typo3:cache:delete_typo3temp_cache.php
@@ -6,7 +6,7 @@ task('typo3:cache:delete_typo3temp_cache', function () {
     // Cache dir to delete.
     set('delete_typo3temp_cache__cache_dir', 'typo3temp/var/Cache');
 
-    // set active_dir so the task can be used before or after "symlink" task
+    // Set active_dir so the task can be used before or after "symlink" task.
     if (run('if [ -L {{deploy_path}}/release ] ; then echo true; fi')->toBool()) {
         set('active_dir', get('deploy_path') . '/release');
     } else {
