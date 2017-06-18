9c64d0afbf1a576b32049cdf4779872a17be8585
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-06-19 00:20:57 +0200

    [TASK] add loader for DeployerExtendedTypo3Tasks

--- src/Loader.php
+++ src/Loader.php
@@ -8,9 +8,11 @@ class Loader
     {
         require_once 'recipe/common.php';
 
-        new \SourceBroker\DeployerExtended\Loader();
         new \SourceBroker\DeployerExtendedDatabase\Loader();
         new \SourceBroker\DeployerExtendedMedia\Loader();
+        new \SourceBroker\DeployerExtendedTypo3Tasks\Loader();
+        new \SourceBroker\DeployerExtended\Loader();
+
 
         \SourceBroker\DeployerExtended\Utility\FileUtility::requireFilesFromDirectoryReqursively(
             dirname((new \ReflectionClass('\SourceBroker\DeployerExtendedTypo3\Loader'))->getFileName()) . '/../deployer/'
