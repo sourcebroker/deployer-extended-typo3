77f625c5faf5ecf286c880fa0f57ed159515d31c
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-06-19 23:33:46 +0200

    [TASK] Documentation and code cleanup

--- deployer/set.php
+++ deployer/set.php
@@ -2,36 +2,6 @@
 
 namespace Deployer;
 
-use SourceBroker\DeployerExtendedTypo3\Drivers\Typo3EnvDriver;
-
-set('bin/typo3cms', './vendor/bin/typo3cms');
-
-set('current_dir', function () {
-    $current = getcwd();
-    if (is_dir($current) && file_exists($current . '/deploy.php')) {
-        return $current;
-    } else {
-        throw new \RuntimeException('Can not set "current_dir" var. Are you in folder with deploy.php file?');
-    }
-});
-
-// for TYPO3 8.7 we need to have php7
-//set('bin/php', function () {
-//    return run('which php7.1')->toString();
-//});
-//set('composer_options', get('composer_options') . ' --ignore-platform-reqs');
-
-// Array keys for better unsetting if someone would like to change only part of configuration
-add('buffer_config', [
-        'typo3/index.php' => [
-            'entrypoint_filename' => 'typo3/index.php',
-        ],
-        'typo3/cli_dispatch.phpsh' => [
-            'entrypoint_filename' => 'typo3/cli_dispatch.phpsh',
-        ]
-    ]
-);
-
 set('shared_dirs', [
         'fileadmin',
         'uploads',
@@ -40,8 +10,6 @@ set('shared_dirs', [
         'typo3temp/var/logs',
     ]
 );
-
-set('file_remove2steps_items', ['typo3temp/Cache']);
 
 set('shared_files', ['.env']);
 
@@ -64,6 +32,21 @@ set('clear_paths', [
     'dynamicReturnTypeMeta.json'
 ]);
 
+// Look on https://github.com/sourcebroker/deployer-extended#file-rm2steps-1 for docs
+set('file_remove2steps_items', ['typo3temp/Cache']);
+
+// Look on https://github.com/sourcebroker/deployer-extended#buffer-start for docs
+set('buffer_config', [
+        'typo3/index.php' => [
+            'entrypoint_filename' => 'typo3/index.php',
+        ],
+        'typo3/cli_dispatch.phpsh' => [
+            'entrypoint_filename' => 'typo3/cli_dispatch.phpsh',
+        ]
+    ]
+);
+
+// Look https://github.com/sourcebroker/deployer-extended-media for docs
 set('media',
     [
         'filter' => [
@@ -76,8 +59,9 @@ set('media',
         ]
     ]);
 
+// Look https://github.com/sourcebroker/deployer-extended-database for docs
 set('db_default', [
-    'caching_tables' => [
+    'truncate_tables' => [
         'cf_.*'
     ],
     'ignore_tables_out' => [
@@ -102,16 +86,20 @@ set('db_default', [
         'tx_crawler_queue',
         'tx_crawler_process',
     ],
-    'ignore_tables_in' => [],
-    'post_sql_out' => '',
-    'post_sql_in' => ''
+    'post_sql_in' => '',
+    'post_sql_in_markers' => '
+                              UPDATE sys_domain SET hidden = 1;
+                              UPDATE sys_domain SET sorting = sorting + 10;
+                              UPDATE sys_domain SET sorting=1, hidden = 0 WHERE domainName IN ({{domainsSeparatedByComma}});
+                              '
 ]);
 
+// Look https://github.com/sourcebroker/deployer-extended-database for docs
 set('db_databases',
     [
         'database_default' => [
             get('db_default'),
-            (new Typo3EnvDriver)->getDatabaseConfig(
+            (new \SourceBroker\DeployerExtendedTypo3\Drivers\Typo3EnvDriver)->getDatabaseConfig(
                 [
                     'host' => 'TYPO3__DB__Connections__Default__host',
                     'port' => 'TYPO3__DB__Connections__Default__port',
@@ -120,13 +108,6 @@ set('db_databases',
                     'password' => 'TYPO3__DB__Connections__Default__password',
                 ]
             ),
-            [
-                'post_sql_in_markers' => '
-                              UPDATE sys_domain SET hidden = 1;
-                              UPDATE sys_domain SET sorting = sorting + 10;
-                              UPDATE sys_domain SET sorting=1, hidden = 0 WHERE domainName IN ({{domainsSeparatedByComma}});
-                              '
-            ]
         ]
     ]
 );
--- src/Drivers/Typo3EnvDriver.php
+++ src/Drivers/Typo3EnvDriver.php
@@ -12,9 +12,11 @@ use SourceBroker\DeployerExtended\Utility\FileUtility;
 class Typo3EnvDriver
 {
     /**
-     * @param null $params
+     * @param null $dbMappingFields
+     * @param null $absolutePathWithConfig
      * @return array
      * @throws \Exception
+     * @internal param null $params
      */
     public function getDatabaseConfig($dbMappingFields = null, $absolutePathWithConfig = null)
     {
@@ -35,76 +37,76 @@ class Typo3EnvDriver
     /**
      * @param $params
      */
-    public function createEnvFileIfDoesNotExist($params)
-    {
-        if (!file_exists(FileUtility::normalizeFolder($params['configDir']) . '/.env')) {
-
-            if (getenv('TYPO3_CONTEXT') == false
-                || getenv('INSTANCE') == false
-                || getenv('DATABASE_HOST') == false
-                || getenv('DATABASE_PASSWORD') == false
-                || getenv('DATABASE_USER') == false
-                || getenv('DATABASE_PORT') == false
-            ) {
-                throw new ConfigurationException('Create .env file yourself from .env.dist or it can be done automaticly for you. Read the docs. 
-                Just pass TYPO3_CONTEXT_ENV, INSTANCE, DATABASE_HOST, DATABASE_PASSWORD, DATABASE_USER, DATABASE_PORT, TYPO3__GFX__im_path, TYPO3__GFX__im_path_lzw, TYPO3__GFX__colorspace as env.');
-            }
-
-            if (getenv('DATABASE_NAME') === false) {
-                exec('cd ' . $params['configDir'] . ' && basename `git rev-parse --show-toplevel`', $output);
-                if (isset($output[0]) && strlen($output[0]) == 0) {
-                    throw new ConfigurationException('Can not get git repo name from "basename `git rev-parse --show-toplevel`" command. Its needed to create database.');
-                }
-                $databaseBaseName = 'typo3_' . $output[0];
-            } else {
-                $databaseBaseName = getenv('DATABASE_NAME');
-            }
-            $host = getenv('DATABASE_HOST');
-            $username = getenv('DATABASE_USER');
-            $password = getenv('DATABASE_PASSWORD');
-            $port = getenv('DATABASE_PORT');
-            $databaseName = '';
-            try {
-                $mysqli = new \mysqli($host, $username, $password);
-                $databaseCreated = false;
-                $i = 0;
-                while ($databaseCreated == false && $i < 3) {
-                    $databaseName = $databaseBaseName . ($i ? '_' . $i : '');
-                    $mysqli->query("CREATE DATABASE IF NOT EXISTS " . $databaseName);
-                    $result = $mysqli->query("SELECT COUNT(DISTINCT `table_name`) AS table_counter FROM `information_schema`.`columns` WHERE `table_schema` = '" . $databaseName . "'");
-                    if (intval($result->fetch_assoc()['table_counter']) === 0) {
-                        $databaseCreated = true;
-                    } else {
-                        $i++;
-                    }
-                }
-            } catch (\mysqli_sql_exception $e) {
-                throw $e;
-            }
-
-            $envDist = file_get_contents(FileUtility::normalizeFolder($params['configDir']) . '/.env.dist');
-
-            $replace = [
-                'TYPO3_CONTEXT' => getenv('TYPO3_CONTEXT'),
-                'INSTANCE' => getenv('INSTANCE'),
-                'DATABASE_NAME' => $databaseName,
-                'DATABASE_HOST' => $host,
-                'DATABASE_PASSWORD' => $password,
-                'DATABASE_PORT' => $port,
-                'DATABASE_USER' => $username,
-                'IMAGE_MAGICK_PATH' => getenv('IMAGE_MAGICK_PATH'),
-                'IMAGE_MAGICK_COLORSPACE' => getenv('IMAGE_MAGICK_COLORSPACE')
-            ];
-
-            $envDistMarkersReplaced = preg_replace_callback(
-                "/\\{([A-Za-z0-9_:]+)\\}/",
-                function ($match) use ($replace) {
-                    return $replace[$match[1]];
-                },
-                $envDist
-            );
-
-            file_put_contents(FileUtility::normalizeFolder($params['configDir']) . '/.env', $envDistMarkersReplaced);
-        }
-    }
+//    public function createEnvFileIfDoesNotExist($params)
+//    {
+//        if (!file_exists(FileUtility::normalizeFolder($params['configDir']) . '/.env')) {
+//
+//            if (getenv('TYPO3_CONTEXT') == false
+//                || getenv('INSTANCE') == false
+//                || getenv('DATABASE_HOST') == false
+//                || getenv('DATABASE_PASSWORD') == false
+//                || getenv('DATABASE_USER') == false
+//                || getenv('DATABASE_PORT') == false
+//            ) {
+//                throw new ConfigurationException('Create .env file yourself from .env.dist or it can be done automaticly for you. Read the docs.
+//                Just pass TYPO3_CONTEXT_ENV, INSTANCE, DATABASE_HOST, DATABASE_PASSWORD, DATABASE_USER, DATABASE_PORT, TYPO3__GFX__im_path, TYPO3__GFX__im_path_lzw, TYPO3__GFX__colorspace as env.');
+//            }
+//
+//            if (getenv('DATABASE_NAME') === false) {
+//                exec('cd ' . $params['configDir'] . ' && basename `git rev-parse --show-toplevel`', $output);
+//                if (isset($output[0]) && strlen($output[0]) == 0) {
+//                    throw new ConfigurationException('Can not get git repo name from "basename `git rev-parse --show-toplevel`" command. Its needed to create database.');
+//                }
+//                $databaseBaseName = 'typo3_' . $output[0];
+//            } else {
+//                $databaseBaseName = getenv('DATABASE_NAME');
+//            }
+//            $host = getenv('DATABASE_HOST');
+//            $username = getenv('DATABASE_USER');
+//            $password = getenv('DATABASE_PASSWORD');
+//            $port = getenv('DATABASE_PORT');
+//            $databaseName = '';
+//            try {
+//                $mysqli = new \mysqli($host, $username, $password);
+//                $databaseCreated = false;
+//                $i = 0;
+//                while ($databaseCreated == false && $i < 3) {
+//                    $databaseName = $databaseBaseName . ($i ? '_' . $i : '');
+//                    $mysqli->query("CREATE DATABASE IF NOT EXISTS " . $databaseName);
+//                    $result = $mysqli->query("SELECT COUNT(DISTINCT `table_name`) AS table_counter FROM `information_schema`.`columns` WHERE `table_schema` = '" . $databaseName . "'");
+//                    if (intval($result->fetch_assoc()['table_counter']) === 0) {
+//                        $databaseCreated = true;
+//                    } else {
+//                        $i++;
+//                    }
+//                }
+//            } catch (\mysqli_sql_exception $e) {
+//                throw $e;
+//            }
+//
+//            $envDist = file_get_contents(FileUtility::normalizeFolder($params['configDir']) . '/.env.dist');
+//
+//            $replace = [
+//                'TYPO3_CONTEXT' => getenv('TYPO3_CONTEXT'),
+//                'INSTANCE' => getenv('INSTANCE'),
+//                'DATABASE_NAME' => $databaseName,
+//                'DATABASE_HOST' => $host,
+//                'DATABASE_PASSWORD' => $password,
+//                'DATABASE_PORT' => $port,
+//                'DATABASE_USER' => $username,
+//                'IMAGE_MAGICK_PATH' => getenv('IMAGE_MAGICK_PATH'),
+//                'IMAGE_MAGICK_COLORSPACE' => getenv('IMAGE_MAGICK_COLORSPACE')
+//            ];
+//
+//            $envDistMarkersReplaced = preg_replace_callback(
+//                "/\\{([A-Za-z0-9_:]+)\\}/",
+//                function ($match) use ($replace) {
+//                    return $replace[$match[1]];
+//                },
+//                $envDist
+//            );
+//
+//            file_put_contents(FileUtility::normalizeFolder($params['configDir']) . '/.env', $envDistMarkersReplaced);
+//        }
+//    }
 }
