cef769986302b0aa484597b93ccf0c6f8a6b1bd3
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-07-26 14:27:12 +0200

    [TASK] new version for TYPO3 6.2

--- src/Loader.php
+++ src/Loader.php
@@ -10,9 +10,7 @@ class Loader
 
         new \SourceBroker\DeployerExtendedDatabase\Loader();
         new \SourceBroker\DeployerExtendedMedia\Loader();
-        new \SourceBroker\DeployerExtendedTypo3Tasks\Loader();
         new \SourceBroker\DeployerExtended\Loader();
-
 
         \SourceBroker\DeployerExtended\Utility\FileUtility::requireFilesFromDirectoryReqursively(
             dirname((new \ReflectionClass('\SourceBroker\DeployerExtendedTypo3\Loader'))->getFileName()) . '/../deployer/'
