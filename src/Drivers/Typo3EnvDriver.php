65e99e74ee560d833c07c60998f8b40ec4196076
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2018-05-27 11:04:42 +0200

    [FEATURE] Autocreate for .env file.

--- src/Drivers/Typo3EnvDriver.php
+++ src/Drivers/Typo3EnvDriver.php
@@ -23,6 +23,9 @@ class Typo3EnvDriver
         $absolutePathWithConfig = null === $absolutePathWithConfig ? getcwd() : $absolutePathWithConfig;
         $absolutePathWithConfig = rtrim($absolutePathWithConfig, DIRECTORY_SEPARATOR);
         $dbSettings = [];
+        if (!file_exists($absolutePathWithConfig . '/.env')) {
+            $this->createEnvFileIfDoesNotExist($absolutePathWithConfig . '/.env');
+        }
         if (file_exists($absolutePathWithConfig . '/.env')) {
             (new Dotenv($absolutePathWithConfig))->load();
             foreach ($dbMappingFields as $key => $dbMappingField) {
@@ -35,78 +38,75 @@ class Typo3EnvDriver
     }
 
     /**
-     * @param $params
+     * @param $absolutePathWithConfig
+     * @throws \Exception
      */
-//    public function createEnvFileIfDoesNotExist($params)
-//    {
-//        if (!file_exists(FileUtility::normalizeFolder($params['configDir']) . '/.env')) {
-//
-//            if (getenv('TYPO3_CONTEXT') == false
-//                || getenv('INSTANCE') == false
-//                || getenv('DATABASE_HOST') == false
-//                || getenv('DATABASE_PASSWORD') == false
-//                || getenv('DATABASE_USER') == false
-//                || getenv('DATABASE_PORT') == false
-//            ) {
-//                throw new ConfigurationException('Create .env file yourself from .env.dist or it can be done automaticly for you. Read the docs.
-//                Just pass TYPO3_CONTEXT_ENV, INSTANCE, DATABASE_HOST, DATABASE_PASSWORD, DATABASE_USER, DATABASE_PORT, TYPO3__GFX__im_path, TYPO3__GFX__im_path_lzw, TYPO3__GFX__colorspace as env.');
-//            }
-//
-//            if (getenv('DATABASE_NAME') === false) {
-//                exec('cd ' . $params['configDir'] . ' && basename `git rev-parse --show-toplevel`', $output);
-//                if (isset($output[0]) && strlen($output[0]) == 0) {
-//                    throw new ConfigurationException('Can not get git repo name from "basename `git rev-parse --show-toplevel`" command. Its needed to create database.');
-//                }
-//                $databaseBaseName = 'typo3_' . $output[0];
-//            } else {
-//                $databaseBaseName = getenv('DATABASE_NAME');
-//            }
-//            $host = getenv('DATABASE_HOST');
-//            $username = getenv('DATABASE_USER');
-//            $password = getenv('DATABASE_PASSWORD');
-//            $port = getenv('DATABASE_PORT');
-//            $databaseName = '';
-//            try {
-//                $mysqli = new \mysqli($host, $username, $password);
-//                $databaseCreated = false;
-//                $i = 0;
-//                while ($databaseCreated == false && $i < 3) {
-//                    $databaseName = $databaseBaseName . ($i ? '_' . $i : '');
-//                    $mysqli->query("CREATE DATABASE IF NOT EXISTS " . $databaseName);
-//                    $result = $mysqli->query("SELECT COUNT(DISTINCT `table_name`) AS table_counter FROM `information_schema`.`columns` WHERE `table_schema` = '" . $databaseName . "'");
-//                    if (intval($result->fetch_assoc()['table_counter']) === 0) {
-//                        $databaseCreated = true;
-//                    } else {
-//                        $i++;
-//                    }
-//                }
-//            } catch (\mysqli_sql_exception $e) {
-//                throw $e;
-//            }
-//
-//            $envDist = file_get_contents(FileUtility::normalizeFolder($params['configDir']) . '/.env.dist');
-//
-//            $replace = [
-//                'TYPO3_CONTEXT' => getenv('TYPO3_CONTEXT'),
-//                'INSTANCE' => getenv('INSTANCE'),
-//                'DATABASE_NAME' => $databaseName,
-//                'DATABASE_HOST' => $host,
-//                'DATABASE_PASSWORD' => $password,
-//                'DATABASE_PORT' => $port,
-//                'DATABASE_USER' => $username,
-//                'IMAGE_MAGICK_PATH' => getenv('IMAGE_MAGICK_PATH'),
-//                'IMAGE_MAGICK_COLORSPACE' => getenv('IMAGE_MAGICK_COLORSPACE')
-//            ];
-//
-//            $envDistMarkersReplaced = preg_replace_callback(
-//                "/\\{([A-Za-z0-9_:]+)\\}/",
-//                function ($match) use ($replace) {
-//                    return $replace[$match[1]];
-//                },
-//                $envDist
-//            );
-//
-//            file_put_contents(FileUtility::normalizeFolder($params['configDir']) . '/.env', $envDistMarkersReplaced);
-//        }
-//    }
+    public function createEnvFileIfDoesNotExist($absolutePathWithConfig)
+    {
+        $host = getenv('MYSQL_HOST');
+        $port = getenv('MYSQL_HOST_PORT') ? getenv('MYSQL_HOST_PORT') : 3306;
+        $databaseName = empty(getenv('MYSQL_DATABASE')) ? "" : getenv('MYSQL_DATABASE');
+        if (empty($databaseName)) {
+            exec('git config --get remote.origin.url', $output);
+            preg_match('/\/([a-zA-Z0-9-]+)\.git/', $output[0], $match);
+            if ((isset($output[0]) && strlen($output[0]) == 0) || empty($match[1])) {
+                throw new \Exception('Can not get git repo name from "git config --get remote.origin.url" command. Its needed to create database.');
+            }
+            $databaseBaseName = 'typo3_' . $match[1];
+            if (!empty(getenv('MYSQL_USER')) && !empty(getenv('MYSQL_PASSWORD'))) {
+                $databaseName = $this->tryToCreateDatabaseIfNotExists($host, $port, getenv('MYSQL_USER'),
+                    getenv('MYSQL_PASSWORD'), $databaseBaseName);
+                $username = getenv('MYSQL_USER');
+                $password = getenv('MYSQL_PASSWORD');
+            }
+            if (empty($databaseName) && !empty(getenv('MYSQL_ROOT_USER')) && !empty(getenv('MYSQL_ROOT_PASSWORD'))) {
+                $databaseName = $this->tryToCreateDatabaseIfNotExists($host, $port, getenv('MYSQL_ROOT_USER'),
+                    getenv('MYSQL_ROOT_PASSWORD'), $databaseBaseName);
+                $username = getenv('MYSQL_ROOT_USER');
+                $password = getenv('MYSQL_ROOT_PASSWORD');
+            }
+        }
+        $envDist = file_get_contents(dirname($absolutePathWithConfig) . '/.env.dist');
+        $replace = [
+            'TYPO3_CONTEXT' => getenv('TYPO3_CONTEXT'),
+            'INSTANCE' => getenv('INSTANCE'),
+            'MYSQL_DATABASE' => $databaseName,
+            'MYSQL_USER' => $username,
+            'MYSQL_PASSWORD' => $password,
+            'MYSQL_HOST' => $host,
+            'MYSQL_HOST_PORT' => $port,
+            'IMAGE_MAGICK_PATH' => getenv('IMAGE_MAGICK_PATH'),
+            'IMAGE_MAGICK_COLORSPACE' => getenv('IMAGE_MAGICK_COLORSPACE')
+        ];
+        $envDistMarkersReplaced = preg_replace_callback(
+            "/\\{([A-Za-z0-9_:]+)\\}/",
+            function ($match) use ($replace) {
+                return $replace[$match[1]];
+            },
+            $envDist
+        );
+        file_put_contents(FileUtility::normalizeFolder($absolutePathWithConfig), $envDistMarkersReplaced);
+    }
+
+    private function tryToCreateDatabaseIfNotExists($host, $port, $user, $password, $databaseBaseName)
+    {
+        $databaseName = null;
+        $mysqli = new \mysqli($host, $user, $password, "", $port);
+        if (!$mysqli->connect_errno) {
+            $i = 0;
+            while ($i < 20) {
+                $databaseName = $databaseBaseName . ($i ? '_' . $i : '');
+                $result = $mysqli->query("SELECT COUNT(DISTINCT `table_name`) AS table_counter FROM `information_schema`.`columns` WHERE `table_schema` = '" . $databaseName . "'");
+                if (!empty($result) && intval($result->fetch_assoc()['table_counter']) == 0) {
+                    $mysqli->query("CREATE DATABASE IF NOT EXISTS " . $databaseName);
+                    break;
+                } else {
+                    $i++;
+                }
+            }
+        }
+        return $databaseName;
+    }
 }
+
+
