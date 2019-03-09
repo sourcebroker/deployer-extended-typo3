18767593c7d60c10e1a5fd48da827cb782d6e8c1
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2019-03-09 14:45:34 +0100

    [TASK] Testing separate instance.

--- src/Loader.php
+++ src/Loader.php
@@ -11,6 +11,7 @@ class Loader
         /** @noinspection PhpIncludeInspection */
         require_once 'recipe/common.php';
         new Load([
+                ['path' => 'vendor/sourcebroker/deployer-instance/deployer'],
                 ['path' => 'vendor/sourcebroker/deployer-extended/deployer'],
                 ['path' => 'vendor/sourcebroker/deployer-extended-database/deployer'],
                 ['path' => 'vendor/sourcebroker/deployer-extended-media/deployer'],
