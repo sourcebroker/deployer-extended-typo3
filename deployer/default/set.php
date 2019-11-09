8f029e5a97541a6001ec6b04e496904769639fbb
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2019-11-09 12:09:39 +0100

    [TASK] Remove unused settings.

--- deployer/default/set.php
+++ deployer/default/set.php
@@ -30,12 +30,6 @@ set('clear_paths', [
 set('bin/typo3cms', './vendor/bin/typo3cms');
 set('local/bin/typo3cms', './vendor/bin/typo3cms');
 
-// Look on https://github.com/sourcebroker/deployer-extended#file-rm2steps-1 for docs
-set('file_remove2steps_items', [
-    'typo3temp/Cache',
-    'typo3temp/var/Cache'
-]);
-
 // Look https://github.com/sourcebroker/deployer-extended-media for docs
 set('media',
     [
